6189c2f0bc5078fb78c40b129f74e9503f76ee93
Kuto <86799382+Kutopus@users.noreply.github.com>
2021-11-27 17:30:02 -0300

    Update question_6.py

--- question_6.py
+++ question_6.py
@@ -12,7 +12,7 @@ url = 'https://metrobi.com/'
 browser.get(url)
 browser.save_screenshot('metrobi.png')
 
-x, y, z, w = pg.locateOnScreen('metrobi_fundo.png', grayscale=True, confidence=.5)
+x, y, z, w = pg.locateOnScreen('metrobi_fundo.png', grayscale=True, confidence=.9)
 
 print(f"Coordinates:\nLeft (X): {x} | Top (Y): {y}")
 
