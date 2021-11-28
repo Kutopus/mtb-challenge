260affc0626175c74942d7d4ad9bffe77113706f
Kuto <86799382+Kutopus@users.noreply.github.com>
2021-11-28 11:55:22 -0300

    Update question_6_detection.py

--- question_6_detection.py
+++ question_6_detection.py
@@ -8,27 +8,29 @@ s = Service(r'C:\Programação\Python\chromedriver.exe')
 
 browser = webdriver.Chrome(service=s)
 # You can change the windows size
-size_x, size_y = 640, 1136
+size_x, size_y = 1920, 1080
 browser.set_window_size(size_x, size_y)
 
 # You can change the URL too
 # (https://metrobi.com / https://deliver.metrobi.com/signin)
-url = 'https://metrobi.com'
+url = 'https://deliver.metrobi.com/signin'
 
 browser.get(url)
 browser.save_screenshot('metrobi.png')
 
 browser.close()
 
-img = cv2.imread('metrobi.png')
+img = cv2.imread('metrobi.png', cv2.COLOR_GRAY2BGR)
 cv2.setWindowProperty("Image", cv2.WINDOW_AUTOSIZE, cv2.WINDOW_KEEPRATIO)
 cv2.imshow("Image", img)
 cv2.waitKey(500)
 
 try:
-    x, y, z, w = pg.locateOnScreen('metrobi_fundo.png', grayscale=True, confidence=.5)
+    x, y, z, w = pg.locateOnScreen('metrobi_fundo.png', grayscale=True, confidence=.9)
 except TypeError:
-    x, y, z, w = pg.locateOnScreen('metrobi_fundo_2.png', grayscale=True, confidence=.5)
+    x, y, z, w = pg.locateOnScreen('metrobi_fundo_2.png', grayscale=True, confidence=.6)
+except Exception:
+    print("I can't find that, try to change the confidence :)")
 
 print(f"Coordinates:\nLeft (X): {x} | Top (Y): {y}")
 
