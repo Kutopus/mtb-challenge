4cc274771fa1b77302351c8a72dc0d1e7cebc0f9
Kuto <86799382+Kutopus@users.noreply.github.com>
2021-11-28 11:20:42 -0300

    Update question_6.py

--- question_6.py
+++ question_6.py
@@ -1,22 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import pyautogui as pg
+import cv2
+import time
 
+# Your Chromedriver path
 s = Service(r'C:\Programação\Python\chromedriver.exe')
 
 browser = webdriver.Chrome(service=s)
-browser.set_window_size(1920, 1080)
+# You can change the windows size
+size_x, size_y = 1920, 1080
+browser.set_window_size(size_x, size_y)
 
-url = 'https://metrobi.com/'
+# You can change the URL too
+# (https://metrobi.com / https://deliver.metrobi.com/signin)
+url = 'https://metrobi.com'
 
 browser.get(url)
 browser.save_screenshot('metrobi.png')
 
-x, y, z, w = pg.locateOnScreen('metrobi_fundo.png', grayscale=True, confidence=.9)
+browser.close()
+
+img = cv2.imread('metrobi.png')
+cv2.namedWindow("Image", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.imshow("Image", img)
+
+try:
+    x, y, z, w = pg.locateOnScreen('metrobi_fundo.png', grayscale=True, confidence=.5)
+except TypeError:
+    x, y, z, w = pg.locateOnScreen('metrobi_fundo_2.png', grayscale=True, confidence=.5)
 
 print(f"Coordinates:\nLeft (X): {x} | Top (Y): {y}")
 
-# To test
-# pg.click(x, y)
+pg.moveTo(x, y)
 
-browser.close()
+print('----------------------------')
+
+cv2.destroyAllWindows()
